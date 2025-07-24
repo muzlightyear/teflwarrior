8e3257ed811615d573a5f3a139a77451e44f1e3e
Murray <murraymclellan@hotmail.co.uk>
2025-07-24 22:56:02 +0700

    Makeover #1

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,12 @@
-import Timer from '../Timer/Timer.js';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <div className='navBar'>
-            <h1>TEFL Warrior</h1><br />
+            <h1>TEFL Warrior</h1>
                 <div className='links'>
                     <Link to='/'> Home</Link>
-                    <a>Timer</a>
                     <Link to='/management'>Student Management</Link>
                 </div>
         </div>
--- src/components/RandomizeRibbon/Randomizer.js
+++ src/components/RandomizeRibbon/Randomizer.js
@@ -1,45 +1,9 @@
 import './Randomizer.css';
-import { useState } from 'react';
-import { describeArray, drawArray, playArray }  from './Arrays.js';
 
 function Randomizer() {
-   let randomizeArray = [
-      "Randomize Me Again...",
-      "Try Randomizing Me...",
-      "Randomized Text Goes Here",
-      "Randomize Me..."
-   ]
-
-   const [result, setResult] = useState(randomizeArray[3]);
-   const [selection,setSelection] = useState("Describe:");
-
-
-   function resetResult(e) {
-     setResult(randomizeArray[Math.floor(Math.random() * randomizeArray.length)]);
-      setSelection(e.currentTarget.value);
-   }
-
-   function randomButtonClick() {
-      if (selection === "Describe:"){
-         setResult(describeArray[Math.floor(Math.random() * describeArray.length)]);
-      }else if (selection === "Draw:"){
-         setResult(drawArray[Math.floor(Math.random() * describeArray.length)]);
-      }else {
-         setResult(playArray[Math.floor(Math.random() * describeArray.length)]);
-      }
-   }
-
    return (
       <div className='randomizer'>
-      <div className="ribbon">
-        <select className="dropdown" onChange={resetResult} >
-            <option id="describe">Describe:</option>
-            <option id="draw">Draw:</option>
-            <option id="play">Play:</option>
-         </select>
-         <span className='answer'>{result}</span>
-   </div>
-   <button className='randomize' onClick={randomButtonClick}>Randomize</button>
+         <button className='randomize'>Randomize</button>
    </div>
    )
 }
